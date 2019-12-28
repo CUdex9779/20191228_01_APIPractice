e7c9d89e850e8ea33eb6c10f07676b5e6e62d5c1
CUdex9779 <cuyu9779@gmail.com>
2019-12-28 14:15:38 +0900

    스플레쉬 액티비티 생성 - 토큰 저장 여부 확인 => 어느 화면으로 이동시킬지 결정 - 너무 빨리 동작하는게 어색해서 postDelayed활용

---
+++ app/src/main/java/com/example/a20191228_01_apipractice/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.a20191228_01_apipractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        setValues()
+        setEvents()
+    }
+
+    override fun setEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
