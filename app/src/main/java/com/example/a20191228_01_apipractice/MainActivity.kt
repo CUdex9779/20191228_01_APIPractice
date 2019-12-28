a046362d912a3cce816c3b0e2938e40bc8d4f65b
CUdex9779 <cuyu9779@gmail.com>
2019-12-28 16:39:52 +0900

    JSONArray안의 JSONobject 추가

--- app/src/main/java/com/example/a20191228_01_apipractice/MainActivity.kt
+++ app/src/main/java/com/example/a20191228_01_apipractice/MainActivity.kt
@@ -2,8 +2,16 @@ package com.example.a20191228_01_apipractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.telecom.ConnectionService
+import android.widget.Toast
+import com.example.a20191228_01_apipractice.datas.User
+import com.example.a20191228_01_apipractice.utils.ConnentServer
+import org.json.JSONObject
+import java.net.ConnectException
 
 class MainActivity : BaseActivity() {
+
+    var userList = Array<User>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +27,41 @@ class MainActivity : BaseActivity() {
 
     override fun setValues() {
 
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        ConnentServer.getRequestUserList(mContext,object : ConnentServer.JsonResponseHandler{
+            override fun onResponse(json: JSONObject) {
+
+                val code = json.getInt("code")
+                runOnUiThread{
+
+                    if (code == 200){
+
+                        val data = json.getJSONObject("data")
+                        val users = data.getJSONArray("users")
+
+                        for (i in 0..(users.length()-1)){
+                            val userJson = users.getJSONObject(i)
+
+                            val userDataObject = User.getUserFromJson(userJson)
+
+
+
+                        }
+
+                    }
+                    else{
+                        Toast.makeText(mContext,"서버에문제가있습니다.",Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+
+            }
+
+        })
     }
 
 
